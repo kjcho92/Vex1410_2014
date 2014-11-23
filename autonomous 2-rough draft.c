265cfa5f0e450f4fde42ea9df212cd2bcb93b2c4
Simon <fujizhang2525@gmail.com>
2014-11-23 11:43:10 -0800

    Create autonomous 2-rough draft.c

---
+++ autonomous 2-rough draft.c
@@ -0,0 +1,17 @@
+task autonomous() // this code is for blue team-- the robot starts in the non- skyrise tile and is facing opposite of the wall
+{ // most values will not be here, instead fillers (#) will be placed
+
+  int valueA = x; //power levels 
+  int valueB = y;
+  
+    if (SensorValue[Jumper1] == 0) // im guessing this is for the different autonomous? 
+    {
+      MovingFoward = false; //because this will use the claw, thought it would be easier to code if i do this
+      { // move fowards
+          
+        	SensorValue[EncoderFrontRight] = 0;
+          while(SensorValue[EncoderFrontRight] <= # )
+          {
+            RightEdge(valueA);
+          }
+          
