8adf8c5f4e2ebeef0349cfe156dbb7ac5dc09643
Simon <fujizhang2525@gmail.com>
2014-11-23 16:20:33 -0800

    Update autonomous 2-rough draft.c

--- autonomous 2-rough draft.c
+++ autonomous 2-rough draft.c
@@ -4,7 +4,7 @@ task autonomous() // this code is for blue team-- the robot starts in the non- s
   int valueA = x; //power levels 
   int valueB = y;
   
-    if (SensorValue[Jumper1] == 0) // im guessing this is for the different autonomous? 
+    if (SensorValue[Jumper2] == 0) // im guessing this is for the different autonomous? 
     {
       MovingFoward = false; //because this will use the claw, thought it would be easier to code if i do this
       { // move foward untill you reach that cube
@@ -35,7 +35,20 @@ task autonomous() // this code is for blue team-- the robot starts in the non- s
           while(SensorValue[armPotentiometerLeft] < savedLeftValue + moveUpTo
             && SensorValue[armPotentiometerRight] < savedRightValue + moveUpTo)
           {
-            Lift(#); //Lift goes up
+            Lift(-#); //Lift goes up
           }
           Lift (0);
           wait1Msec(#);
+          
+          GyroRotate( # );
+          wait1Msec(#);
+          
+          while(SensorValue[armPotentiometerLeft] < savedLeftValue + moveUpTo
+            && SensorValue[armPotentiometerRight] < savedRightValue + moveUpTo)
+          {
+            Lift(#); //Lift goes up
+          }
+          Lift (0);
+          wait1Msec(#); // add less delay so claw releases whilemoving down
+          
+          cubeIntakeClosing = false;
